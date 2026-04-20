d0cab00c2ed55024c746c4c0d3ae960bd5323380
HrishavSaha <hrishav.saha@gmail.com>
2026-04-21 02:06:13 +0530

    docs: replace placeholder captions with actual descriptions
    Update gallery image captions from lorem ipsum placeholder text to meaningful descriptions that accurately represent each photograph. This improves the user experience by providing context and information about the images in the gallery.

--- data/images.ts
+++ data/images.ts
@@ -10,132 +10,102 @@ export const IMAGES: GalleryImage[] = [
     src: "/images/1.webp",
     alt: "Alt for image 1",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "A heavy snowstorm in Washington, D.C. obscures open-air entrances of Metro stations, making the commute difficult for essential employees required to be in their offices.",
   },
   {
     src: "/images/2.webp",
     alt: "Alt for image 2",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "A performer rehearses an aerial act inside Washington's New York School of Trapeze, erected in the Navy Yard neighborhood.",
   },
   {
     src: "/images/3.webp",
     alt: "Alt for image 3",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
-  },
-  {
-    src: "/images/4.webp",
-    alt: "Alt for image 4",
-    caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "Space Shuttle Discovery, atop a NASA 747 Shutte Carrier Aircraft, makes its final flyover of the Washington region on the way to the Smithsonian’s Udvar-Hazy Air and Space Museum annex in Chantilly, Va., April 17, 2012.",
   },
   {
     src: "/images/5.webp",
     alt: "Alt for image 5",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "A cyclist passes a preserved section of the Berlin Wall along Niederkirchnerstraße."
   },
   {
     src: "/images/6.webp",
     alt: "Alt for image 6",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "An actor with a Washington improv troupe performs the last moments of his celebratory routine.",
   },
   {
     src: "/images/7.webp",
     alt: "Alt for image 7",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "A bicycle counter in Copenhagen displays a running tally of daily and annual ridership. The city has long promoted cycling as a primary mode of transportation.",
   },
   {
     src: "/images/8.webp",
     alt: "Alt for image 8",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "A statue of the Virgin Mary stands against a painted wall in La Boca, Buenos Aires. This melting pot of a neighborhood became a stronghold for porteño culture when millions of European and Asian immigrants arrived in the early 20th century.",
   },
   {
     src: "/images/9.webp",
     alt: "Alt for image 9",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
-  },
-  {
-    src: "/images/10.webp",
-    alt: "Alt for image 10",
-    caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
-  },
-  {
-    src: "/images/11.webp",
-    alt: "Alt for image 11",
-    caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "A couple walks along Rynisfjara black sand beach near Vík, Iceland, during a winter storm.",
   },
   {
     src: "/images/12.webp",
     alt: "Alt for image 12",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "Attendees of President Barack Obama's first inauguration find themselves atop a line of portable restrooms. Record attendance in 2009 forced some to find alternate ways of navigating crowds in deadlock.",
   },
   {
     src: "/images/13.webp",
     alt: "Alt for image 13",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      `"The War Room:" three members of the wedding party strategize in a makeshift bullpen at the home of the bride's parents.`,
   },
   {
     src: "/images/14.webp",
     alt: "Alt for image 14",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "Glacier guide Nick Klco leads a team of visitors to explore the base of Matanuska Glacier, approximately 100 miles north of Anchorage, Ak.",
   },
   {
     src: "/images/15.webp",
     alt: "Alt for image 15",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "Campers sit around a fire on Assateague Island, Md. The group was disturbed several times throughout the night by the feral horses for which the island is famous.",
   },
   {
     src: "/images/16.webp",
     alt: "Alt for image 16",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "A throng of celebrants head northward on 14th Street, NW in Washington, following the first inauguration of President Barack Obama.",
   },
   {
     src: "/images/17.webp",
     alt: "Alt for image 17",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
-  },
-  {
-    src: "/images/18.webp",
-    alt: "Alt for image 18",
-    caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "Firefighter recruits at the Washington, D.C. Fire Department's Training Academy practice advancing a hoseline as a team.",
   },
   {
     src: "/images/19.webp",
     alt: "Alt for image 19",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
-  },
-  {
-    src: "/images/20.webp",
-    alt: "Alt for image 20",
-    caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "Hosted every year at the Eisenhower National Historic Site in Gettysburg, Pa., “World War II Weekend” attracts reenactors of all ages in period-appropriate costumes.",
   },
   {
     src: "/images/21.webp",
     alt: "Alt for image 21",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "Members of a firefighting recruit class are treated to a tour of the U.S. Capitol Building. Knowledge of the interior of the numerous government buildings is crucial information for those responding to the often maze-like interior.",
   },
   {
     src: "/images/22.webp",
     alt: "Alt for image 22",
     caption:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat porta ipsum ut tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque pulvinar elit blandit bibendum convallis. Morbi ut ante vel tellus scelerisque tincidunt. Cras sit amet ex finibus, porta justo vel, facilisis metus. Integer.",
+      "D.C. Fire & EMS Recruit Class #360 invited former NFL running back John Riggins (of then-Redskins fame) as the motivational speaker at graduation. Also in attendance was his daughter, who found the ceremonial signed firefighting helmet particularly interesting.",
   },
 ];
